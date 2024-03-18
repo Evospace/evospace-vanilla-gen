1adeafc5483ac078b6c060df78ab882efaba2c19
wavelet-noise <2902221+wavelet-noise@users.noreply.github.com>
2024-03-19 00:29:16 +0200

    Update MachinesList.py

--- MachinesList.py
+++ MachinesList.py
@@ -130,7 +130,7 @@ machines = [
 		"CommonTextKeys":[
 			"Autocrafter"
 		],
-		"BlockLogic":"NuclearReactorBlockLogic",
+		"BlockLogic":"SelectCrafter",
 		"BlockCreation":"""
 		return function(self)
 		
@@ -1560,7 +1560,7 @@ machines = [
 			[0,-1,2],[-1,-1,2],[-2,-1,2],[-3,-1,2],
 			[0,-2,2],[-1,-2,2],[-2,-2,2],[-3,-2,2],
 		],
-		"BlockLogic":"NuclearReactorBlockLogic",
+		"BlockLogic":"SelectCrafter",
 		"BlockCreation":"""
 		return function(self)
 		
