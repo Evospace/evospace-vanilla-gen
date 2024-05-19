eb294834ab80fc8a8b882d0ecffb7fb2ef24f982
wavelet-noise <ishellstrike@gmail.com>
2024-05-19 23:39:34 +0300

    instancing for blocks; energy crystal

--- Generators/Covers.py
+++ Generators/Covers.py
@@ -1,53 +1,57 @@
 from Common import *
 
 paint_colors = ("PaintYellow",)
-paint_metals = ("Stone", "Copper", "Steel", "Aluminium", "StainlessSteel", "Titanium", "HardMetal", "Neutronium")
+paint_metals = ("Copper", "Steel", "Aluminium", "StainlessSteel", "Titanium", "HardMetal", "Neutronium")
+paint_tiers = ("Stone", "Copper", "Steel", "Aluminium", "StainlessSteel", "Titanium", "HardMetal", "Neutronium")
+tier_materials = ("Materials/Stone", "Materials/Copper", "Materials/Steel", "Materials/Aluminium", "Materials/StainlessSteel", "Materials/Titanium", "Materials/HardMetal", "Materials/Neutronium")
 
 covers = [
     {
         "Name": "CableSide",
         "Mesh": "Covers/PipeSide",
-        "Materials": ["Materials/Rubber2"],
-        "Item": "CopperConnector"
+        "Materials": ["Materials/Rubber2"]
     },
     {
         "Name": "CableCenter",
         "Mesh": "Covers/PipeCenter",
-        "Materials": ["Materials/Rubber2"],
-        "Item": "CopperConnector"
+        "Materials": ["Materials/Rubber2"]
     },{
         "Name": "HeatSide",
         "Mesh": "Covers/HeatSide",
-        "Materials": ["Materials/HeatingCopper"],
-        "Item": "CopperHeatPipe"
+        "Materials": ["Materials/HeatingCopper"]
     },{
         "Name": "HeatCenter",
         "Mesh": "Covers/HeatCenter",
-        "Materials": ["Materials/HeatingCopper"],
-        "Item": "CopperHeatPipe"
+        "Materials": ["Materials/HeatingCopper"]
     },{
 		"Name": "Cover",
 		"Mesh": "Cover",
-        "Materials": [""],
-        "Item": "Cover"
+        "Materials": [""]
+	},{
+		"Name": "StoneFurnace",
+		"Mesh": "019Content/FurnaceRound",
+        "Materials": ["019Content/StoneFurnaceMaterial"]
+	},{
+		"Name": "RobotArmBase",
+		"Mesh": "019Content/RobotArmBase",
+        "Materials": []
 	}
 ]
 
 objects_array = []
 
-for mat in paint_colors + paint_metals:
+for mat in paint_colors + paint_tiers:
 	covers.append({
 		"Name": "Cover" + mat,
 		"Mesh": "Cover",
         "Materials": [mat]
 	})
      
-for mat, num in zip(paint_metals, range(0,7)):
+for mat, num in zip(paint_tiers, range(0,7+1)):
     covers.append({
         "Name": mat+"PipeCenter",
         "Mesh": "Covers/PipeCenter",
-        "Materials": ["Materials/"+mat],
-        "Item": mat+"Pipe"
+        "Materials": ["Materials/"+mat]
     })
     covers.append({
         "Name": mat+"PipeSide",
@@ -58,21 +62,32 @@ for mat, num in zip(paint_metals, range(0,7)):
     covers.append({
         "Name": mat+"Corner",
         "Mesh": "CoreContent/Corner",
-        "Materials": ["Materials/"+mat],
-        "Item": mat+"Corner"
+        "Materials": ["Materials/"+mat]
     })
     covers.append({
         "Name": mat+"Scaffold",
         "Mesh": "Models/Scafold",
-        "Materials": ["Materials/"+mat],
-        "Item": mat+"Scaffold"
+        "Materials": ["Materials/"+mat]
     })
+
+for mat, num in zip(paint_metals, range(1,7+1)):
     covers.append({
 		"Name": mat+"Furnace",
 		"Mesh": "019Content/FurnaceRound",
         "Materials": ["019Content/FurnaceMaterial"],
-        "Item": mat+"Furnace",
-        "TierTint": num
+        "Tier": num
+	})
+    covers.append({
+		"Name": mat+"Conveyor",
+		"Mesh": "019Content/conveyor_end",
+        "Materials": ["Materials/RubberWithSpeedParam", tier_materials[num]],
+        "Tier": num
+	})
+    covers.append({
+		"Name": mat+"ConveyorBase",
+		"Mesh": "019Content/conveyor",
+        "Materials": ["Materials/RubberWithSpeedParam", tier_materials[num]],
+        "Tier": num
 	})
 	
 for cover in covers:
@@ -93,12 +108,9 @@ for cover in covers:
         "Mesh": cover["Mesh"],
         "Materials": cover["Materials"]
     }
-    if "TierTint" in cover:
-         staticCover["TierTint"] = cover["TierTint"]
+    if "Tier" in cover:
+         staticCover["Tier"] = cover["Tier"]
 
-    if "Item" in cover:
-        staticCover["Minable"] = {"MiningTime": 20, "Result": cover["Item"] + static_item}
-        staticCover["Item"] = cover["Item"] + static_item
     objects_array.append(staticCover)
 
 
