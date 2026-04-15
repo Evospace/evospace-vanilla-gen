b2280ea35829fd4dacf7e5ddf2981bab31916984
wavelet-noise <ishellstrike@gmail.com>
2026-04-16 02:35:42 +0300

    old comments

--- Generators/Materials.py
+++ Generators/Materials.py
@@ -45,11 +45,6 @@ def named_material(name):
 	
 	raise Exception(f"{name} is not a material")
 	
-# https://ru.wikipedia.org/wiki/%D0%9D%D0%B8%D1%82%D1%80%D0%B8%D0%B4_%D0%B1%D0%BE%D1%80%D0%B0 BoronNitride
-# https://en.wikipedia.org/wiki/Neutronium
-
-# https://ru.wikipedia.org/wiki/%D0%A3%D0%B4%D0%B5%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F_%D1%82%D0%B5%D0%BF%D0%BB%D0%BE%D1%82%D0%B0_%D1%81%D0%B3%D0%BE%D1%80%D0%B0%D0%BD%D0%B8%D1%8F burning
-
 tiered_parts_list_no_dust = ["Plate", "Block", "Parts", "SolarCell"]
 tiered_parts_list = tiered_parts_list_no_dust + ["Dust"]
 
